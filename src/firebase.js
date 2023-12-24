acc670c37de316482d8c639b0b828f0d62017808
Brenini <114372894+BrennoVsantos@users.noreply.github.com>
2023-12-24 14:24:21 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKWz0ayC3KpG3ghQkpe5JCwzMeNiaJqm8",
-  authDomain: "dinseyplusclone.firebaseapp.com",
-  projectId: "dinseyplusclone",
-  storageBucket: "dinseyplusclone.appspot.com",
-  messagingSenderId: "86668729944",
-  appId: "1:86668729944:web:0578fec1a0581d49ff342d",
-  measurementId: "G-VE6MBPMKTL"
+  //Aqui ficam os dados do seu firebase
 };
 
 const firebaseapp = firebase.initializeApp(firebaseConfig);
